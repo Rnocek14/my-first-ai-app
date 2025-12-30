517c09ef34fde3a87451f13a331e2cc1776ba0ae
Rnocek14 <rileynocek663@gmail.com>
2025-12-30 15:25:24 -0600

    Add a Warning card to the DashboardPage

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -27,6 +27,11 @@ const DashboardPage: React.FC = () => {
           <h2 className='text-lg font-semibold'>Total Projects</h2>
           <p className='text-2xl font-bold'>56</p>
         </Card>
+        {/* Warning Card */}
+        <Card className='bg-[#FFCC00] border-[#000000] text-[#111111]'>
+          <h2 className='text-lg font-semibold'>Warning</h2>
+          <p className='text-md'>Please be aware of the upcoming changes!</p>
+        </Card>
       </div>
     </AppShell>
   );
