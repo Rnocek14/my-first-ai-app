6251eba011f509034be509c8b52567a2ac9354be
Rnocek14 <rileynocek663@gmail.com>
2025-12-30 15:50:46 -0600

    Refactor ButtonFixerTest page to use proper components

--- src/pages/ButtonFixerTest.tsx
+++ src/pages/ButtonFixerTest.tsx
@@ -5,13 +5,17 @@ import { Input } from '@/components/ui/input';
 import { Button } from '@/components/ui/button';
 
 const ButtonFixerTest: React.FC = () => {
+  const handleButtonClick = () => {
+    alert('Button clicked!');
+  };
+
   return (
     <AppShell>
-      <PageHeader title="Button Fixer Test" subtitle="Testing raw HTML elements" />
+      <PageHeader title="Button Fixer Test" subtitle="Testing proper elements" />
       <div className='flex flex-col gap-4 p-6'>
-        <Button>Shadcn Button</Button>
-        <Input type='button' value='Raw HTML Button' className='mt-4 p-2 border border-input rounded-md'  />
-        <Input placeholder="This is a raw input"  />
+        <Button onClick={handleButtonClick}>Shadcn Button</Button>
+        <Button variant='outline' className='p-2'>Tailwind Styled Button</Button>
+        <Input placeholder="This is a properly styled input" />
       </div>
     </AppShell>
   );
